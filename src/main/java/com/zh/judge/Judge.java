d9ec486ae3d78572d7ea6d8e430bb5b2609f0c8e
jianghangqing <2233543613@qq.com>
2022-01-25 10:36:00 +0800

    add: Judge

---
+++ src/main/java/com/zh/judge/Judge.java
@@ -0,0 +1,11 @@
+package com.zh.judge;
+
+public class Judge {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("twenty ");
+        builder.append("one ");
+        builder.append("pilots ").append("band");
+        System.out.println("builder = " + builder.toString());
+    }
+}
